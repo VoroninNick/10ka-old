d6abf6845e529f4c3887497138d75f96efd2a9d5
Vik Ewoods <support@voroninstudio.eu>
2013-02-15 17:51:44 +0200

    Initial Clean Project setup

---
+++ README.mr
@@ -0,0 +1,6 @@
+= 10k Application
+= http://10ka.voroninstudio.eu
+
+
+Developer: Vik Ewoods
+Art: Voronin Nick
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App10k::Application.config.secret_token = '329939afd6f8401428feaa682e004fb080d4ae779ac0af326dfd4d5a07d2cc133549ce57ef175967ceb7d3a6c828ee0867d091161efd6b5a53666ec755311a5c'
