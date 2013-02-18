47eca3524e68ed50e292e275ec01d371d5a51a4c
Vik Ewoods <support@voroninstudio.eu>
2013-02-18 11:35:05 +0200

    Dirty changes

---
+++ app/assets/javascripts/annimation.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $('#main-cat-1').click(function() {
+        $('#menu-cat-1').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-2').click(function() {
+        $('#menu-cat-2').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-3').click(function() {
+        $('#menu-cat-3').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-4').click(function() {
+        $('#menu-cat-4').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-5').click(function() {
+        $('#menu-cat-5').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-6').click(function() {
+        $('#menu-cat-6').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-7').click(function() {
+        $('#menu-cat-7').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-8').click(function() {
+        $('#menu-cat-8').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+
+    $('#main-cat-9').click(function() {
+        $('#menu-cat-9').slideToggle('slow', function() {
+            // Animation complete.
+        });
+    });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ App10k::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'page#index'
 
   # See how all your routes lay out with "rake routes"
 
