e317beb8cc730bccad72b2d693b0335a6cb331ed
Vik Ewoods <support@voroninstudio.eu>
2013-02-21 02:59:50 +0200

    Move banners and sponsor to a helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :fetch_sponsors, :fetch_banners
+
+  def fetch_sponsors
+    @fetch_sponsors ||= Sponsor.all
+  end
+
+  def fetch_banners
+    @fetch_banners ||= Banner.all
+  end
 end
