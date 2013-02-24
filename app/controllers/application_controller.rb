d9e3236b6e0df43d861b3d78a2db27e737714c19
Vik Ewoods <support@voroninstudio.eu>
2013-02-24 22:57:52 +0200

    Stash work on catalog - move forward

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :fetch_sponsors, :fetch_banners
+  helper_method :fetch_sponsors, :fetch_banners, :get_catalogs, :get_parents
 
   def fetch_sponsors
     @fetch_sponsors ||= Sponsor.all
@@ -10,5 +10,20 @@ class ApplicationController < ActionController::Base
 
   def fetch_banners
     @fetch_banners ||= Banner.all
+  end
+
+  def get_childs
+    @catalog ||= Catalog.all
+    @parent ||= ParentCatalog.find_all_by_catalog_id(@catalog)
+    @child ||= ChildCatalog.find_all_by_parent_catalog_id(@parent)
+  end
+
+  def get_parents(catalog_ids)
+    #@catalog ||= Catalog.all
+    @parent ||= ParentCatalog.find_all_by_catalog_id(catalog_ids)
+  end
+
+  def get_catalogs
+    @catalog ||= Catalog.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 # -*- encoding : utf-8 -*-
 App10k::Application.routes.draw do
-  
-  
-
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-
   devise_for :users
-  resources :catalogs
   resources :articles
-  match 'about'               => 'page#about'
-  match 'contact'             => 'contact#new', :as => 'contact', :via => :get
-  match 'contact'             => 'contact#create', :as => 'contact', :via => :post
+  match 'about'                      => 'page#about'
+  match 'contact'                    => 'contact#new', :as => 'contact', :via => :get
+  match 'contact'                    => 'contact#create', :as => 'contact', :via => :post
+  match 'catalog/parent/:id'         => 'catalogs#show_parent', :via => :get
+  match 'catalog/parent/childs/:id'  => 'catalogs#show_child', :via => :get
   root :to => 'page#index'
 end
