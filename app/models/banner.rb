39dfc7cd690b1a3c2e830f486f61665a48b263ef
Vik Ewoods <support@voroninstudio.eu>
2013-04-12 01:31:15 +0300

    Banner model remove validation
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Banner < ActiveRecord::Base
-  attr_accessible :banner, :description, :name
+  attr_accessible :banner, :description, :name, :delete_banner
 
   attr_accessor :delete_banner
   before_validation { self.banner.clear if self.delete_banner == '1' }
@@ -12,9 +12,4 @@ class Banner < ActiveRecord::Base
   has_attached_file :banner, :styles => { :thumb => '150x150>' },
                   :url  => '/assets/banner/:id/:style/:basename.:extension',
                   :path => ':rails_root/public/assets/banner/:id/:style/:basename.:extension'
-
-  # Validate banner presence
-	validates_attachment_presence :banner
-	validates_attachment_size :banner, :less_than => 2.megabytes
-	validates_attachment_content_type :banner, :content_type => %w{'image/jpeg', 'image/png'}
 end
