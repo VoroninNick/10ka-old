c311684b4f47a17a75f8706040522c4613dfbcf5
Vik Ewoods <support@voroninstudio.eu>
2013-04-24 15:43:56 +0300

    Add main catalog path
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ App10k::Application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
   resources :articles
-  match 'articles'                                      => 'articles#index', :as => 'articles'
-  match 'articles/:id'                                  => 'articles#show', :as => 'article_item'
-  match 'about'                                         => 'page#about'
-  match 'contact'                                       => 'contact#new', :as => 'contact', :via => :get
-  match 'contact'                                       => 'contact#create', :as => 'contact', :via => :post
-  match 'catalog'                                       => 'catalogs#index', :via => :get, :as => 'catalog_main'
-  match 'catalog/:id'                                   => 'catalogs#show_parent', :via => :get, :as => 'parent_show'
-  match 'catalog/:parent_id/:id'                        => 'catalogs#show_child', :via => :get, :as => 'child_show'
-  match 'catalog/:parent_id/:child_catalog_id/:id'      => 'catalogs#show_product', :via => :get, :as => 'product_show'
-  root :to                                              => 'page#index'
+  match 'articles'                                                  => 'articles#index', :as => 'articles'
+  match 'articles/:id'                                              => 'articles#show', :as => 'article_item'
+  match 'about'                                                     => 'page#about'
+  match 'contact'                                                   => 'contact#new', :as => 'contact', :via => :get
+  match 'contact'                                                   => 'contact#create', :as => 'contact', :via => :post
+  match 'catalog'                                                   => 'catalogs#index', :via => :get, :as => 'catalog_main'
+  match 'catalog/:id'                                               => 'catalogs#show', :via => :get, :as => 'catalog_show'
+  match 'catalog/:catalog_id/:id'                                   => 'catalogs#show_parent', :via => :get, :as => 'parent_show'
+  match 'catalog/:catalog_id/:parent_id/:id'                        => 'catalogs#show_child', :via => :get, :as => 'child_show'
+  match 'catalog/:catalog_id/:parent_id/:child_catalog_id/:id'      => 'catalogs#show_product', :via => :get, :as => 'product_show'
+  root :to                                                          => 'page#index'
 end
