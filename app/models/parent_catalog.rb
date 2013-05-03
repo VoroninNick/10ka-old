5653cfed057049d1db451aa34d88476861a0eb46
pkorenev <pasha_kor@mail.ru>
2013-05-03 15:35:33 +0300

    fluid

--- app/models/parent_catalog.rb
+++ app/models/parent_catalog.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class ParentCatalog < ActiveRecord::Base
-  attr_accessible :catalog_id, :name, :slug
+  attr_accessible :catalog_id, :name, :description, :slug
 
   validates :name, presence: true
   validates :slug, uniqueness: true, presence: true
