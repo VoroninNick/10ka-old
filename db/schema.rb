95ab15cb0b7b8203655073431100933a37858704
root <root@voroninstudio.eu>
2013-08-07 16:26:40 +0300

    Saving server works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806141337) do
+ActiveRecord::Schema.define(:version => 20130807132618) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20130806141337) do
     t.datetime "banner_updated_at"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.string   "url"
   end
 
   create_table "catalogs", :force => true do |t|
