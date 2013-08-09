2db96b3ba6d719a03e5036586299f345d68a407c
Vik Ewoods <support@voroninstudio.eu>
2013-08-09 10:10:01 +0300

    Fix temp animation for index catalog page
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/assets/javascripts/annimation.js
+++ app/assets/javascripts/annimation.js
@@ -140,4 +140,118 @@ $(document).ready(function() {
             });
         }
     });
+
+    // TEmp actions
+  $('#main-cat-12').click(function() {
+    if(!$('#menu-cat-12').hasClass("visited")){
+      $('#menu-cat-12').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-12").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-12").text("-");
+        }else{
+          $("#status-12").text("+");
+        }
+      });
+    }
+  });
+
+  $('#main-cat-13').click(function() {
+    if(!$('#menu-cat-13').hasClass("visited")){
+      $('#menu-cat-13').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-13").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-13").text("-");
+        }else{
+          $("#status-13").text("+");
+        }
+      });
+    }
+  });
+
+  $('#main-cat-14').click(function() {
+    if(!$('#menu-cat-14').hasClass("visited")){
+      $('#menu-cat-14').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-14").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-14").text("-");
+        }else{
+          $("#status-14").text("+");
+        }
+      });
+    }
+  });
+
+  $('#main-cat-15').click(function() {
+    if(!$('#menu-cat-15').hasClass("visited")){
+      $('#menu-cat-15').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-15").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-15").text("-");
+        }else{
+          $("#status-15").text("+");
+        }
+      });
+    }
+  });
+
+  $('#main-cat-16').click(function() {
+    if(!$('#menu-cat-16').hasClass("visited")){
+      $('#menu-cat-16').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-16").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-16").text("-");
+        }else{
+          $("#status-16").text("+");
+        }
+      });
+    }
+  });
+
+  $('#main-cat-17').click(function() {
+    if(!$('#menu-cat-17').hasClass("visited")){
+      $('#menu-cat-17').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-17").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-17").text("-");
+        }else{
+          $("#status-17").text("+");
+        }
+      });
+    }
+  });
+
+  $('#main-cat-18').click(function() {
+    if(!$('#menu-cat-18').hasClass("visited")){
+      $('#menu-cat-18').slideToggle('slow', function() {
+        // Animation complete.
+      });
+
+      $("#status-18").each(function (i) {
+        if($(this).text() != "-"){
+          $("#status-18").text("-");
+        }else{
+          $("#status-18").text("+");
+        }
+      });
+    }
+  });
+
 });
