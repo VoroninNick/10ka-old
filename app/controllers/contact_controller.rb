abf6e62572a5e3d91cd4dcea1ceba4530e329fc5
pasha <p.korenev@voroninstudio.eu>
2013-12-26 12:24:28 +0200

    v3

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -9,14 +9,14 @@ class ContactController < ApplicationController
     @message = Message.new(params[:message])
     render :inline => @message.inspect
 
-    if @message.valid?
+    if @message.valid? && 1 == 2
       new_message = ContactMailer.new_message(@message)
       new_message.deliver
 
       redirect_to(root_path, :notice => "Message was successfully sent.")
     else
-      flash.now.alert = "Please fill all fields."
-      render :new
+      #flash.now.alert = "Please fill all fields."
+      #render :new
     end
   end
 
