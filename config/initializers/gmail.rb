bf88b55faea47dcc2520f0443021f2f677cf1d71
pasha <p.korenev@voroninstudio.eu>
2013-12-26 14:07:57 +0200

    set config for gmail

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "voroninstudio.eu",
-  :user_name            => "support",
-  :password             => "casper159357*0",
+  :user_name            => "p.korenev",
+  :password             => "pasha#1991",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
