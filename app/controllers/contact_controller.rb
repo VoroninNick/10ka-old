77242c4ba3c920ab6060773efe2835831212c793
pasha <p.korenev@voroninstudio.eu>
2014-02-03 13:05:31 +0200

    cancel email sending and ajax form  handler

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -11,9 +11,9 @@ class ContactController < ApplicationController
 
     if @message.valid?
       new_message = ContactMailer.new_message(@message)
-      new_message.deliver
+      #new_message.deliver
 
-      redirect_to( contact_path, :notice => 'Message was succesfully sent' )
+      #redirect_to( contact_path, :notice => 'Message was succesfully sent' )
 
       #redirect_to(root_path, :notice => "Message was successfully sent.")
     else
